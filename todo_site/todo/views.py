7ff8e788f185b6251ea7239eb895b6a95d836b34
bri wyatt <bri.wyatt@smiledirectclub.com>
2018-11-12 12:08:04 -0600

    added model and template

--- todo_site/todo/views.py
+++ todo_site/todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Restaurants
 
 def index(request):
-    context = {}
+    restaurant_list = Restaurants.objects.order_by('id')
+
+    context = {'restaurant_list': restaurant_list}
+
     return render(request, 'index.html', context)
