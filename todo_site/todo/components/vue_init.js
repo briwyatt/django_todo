0a00470ff6ab603d164bc277e3729056f8c6e082
bri wyatt <bri.wyatt@smiledirectclub.com>
2018-11-27 10:23:32 -0600

    vue instance added to django template

--- todo_site/todo/components/vue_init.js
+++
@@ -1,8 +0,0 @@
-import Vue from '../static/vue/dist/vue.min';
-import App from '../components/App.vue';
-
-new Vue(App).$mount('.vue-root');
-
-if (ENVIRONMENT === 'local') {
-  Vue.config.devtools = true;
-}
